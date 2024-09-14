6389c7cdeabf90a13732e3732de590fb550d6a04
devcow85 <devcow85@gmail.com>
2024-09-14 05:52:33 +0000

    Fix: example name change and alt trainer

---
+++ examples/eva_model_trace.py
@@ -0,0 +1,27 @@
+# This example code for verifying register_hook method (generate HW test vector)
+import os
+import pickle
+
+import torch
+from eva import utils, model
+
+if __name__ == "__main__":
+    utils.set_seed(7)
+    snn_model = model.NCARSNet(16, 1, n_steps = 5)
+    out = snn_model(torch.rand((1,5,1,64,64)))
+    
+    snn_model.register_hook()
+    out = snn_model(torch.rand((1,5,1,64,64)))
+    
+    trace_file_path = 'examples/testvector_v0.pickle'
+    snn_model.save_trace_dict(trace_file_path)
+    
+    with open(trace_file_path, 'rb') as file:
+        trace_dict = pickle.load(file)
+        
+    top_level_key = trace_dict.keys()
+    print("top level keys", top_level_key)
+    print("under level key", trace_dict[list(top_level_key)[0]].keys())
+    
+    os.remove(trace_file_path)
+    
--- examples/modelrun_nmnist.py
+++ examples/modelrun_nmnist.py
@@ -2,7 +2,7 @@ import tonic
 import tonic.transforms as transforms
 from torch.utils.data import DataLoader
 
-from eva.datasets import NCARS
+from eva.eva import EVA
 from eva import transforms as eva_transforms
 from eva import utils, model, loss
 import torch
@@ -13,37 +13,34 @@ if __name__ == "__main__":
     # load dataset
     transform = transforms.Compose([
         eva_transforms.RandomTemporalCrop(time_window = 99000),
-        transforms.Denoise(filter_time=11000),
-        transforms.ToFrame(sensor_size=tonic.datasets.DVSGesture.sensor_size,
+        transforms.ToFrame(sensor_size=tonic.datasets.NMNIST.sensor_size,
                            n_time_bins=5)
     ])
     
-    train_ds = tonic.datasets.DVSGesture(save_to = '/data', 
+    train_ds = tonic.datasets.NMNIST(save_to = '/data', 
                                          train=True, 
                                          transform = transform)
     
     transform = transforms.Compose([
         eva_transforms.TemporalCrop(time_window = 99000),
-        transforms.Denoise(filter_time=11000),
-        transforms.ToFrame(sensor_size=tonic.datasets.DVSGesture.sensor_size,
+        transforms.ToFrame(sensor_size=tonic.datasets.NMNIST.sensor_size,
                            n_time_bins=5)
     ])
     
-    val_ds = tonic.datasets.DVSGesture(save_to = '/data', 
+    val_ds = tonic.datasets.NMNIST(save_to = '/data', 
                                          train=False, 
                                          transform = transform)
     
-    batch_size = 64
+    batch_size = 32
     train_loader = DataLoader(train_ds, shuffle=True, batch_size=batch_size, num_workers=8)
     val_loader = DataLoader(val_ds, shuffle=False, batch_size=batch_size, num_workers=8)
     
-    snn_model = model.DVSGestureNet(5, 1, n_steps = 5)
+    snn_model = model.NMNISTNet(5, 1, n_steps = 5)
     
     optimizer = torch.optim.AdamW(snn_model.parameters(), lr=0.0005)
     spikeloss = loss.SpikeCountLoss(4, 1)
     
     num_epochs = 100  # 원하는 에포크 수 설정
-
-    for epoch in range(num_epochs):
-        utils.train(snn_model, epoch, train_loader, optimizer, spikeloss, device = "cuda")
-        utils.validation(snn_model, val_loader, device = "cuda")
+    
+    snn_eva = EVA(snn_model, optimizer, spikeloss, (train_loader, val_loader), num_epochs, "cuda:1")
+    snn_eva.trainer()
